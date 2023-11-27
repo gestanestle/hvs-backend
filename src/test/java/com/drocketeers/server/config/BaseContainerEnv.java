891687723757830dec3639ebcf97992d8810b190
gestanestle <gesta.nestleo@gmail.com>
2023-11-27 22:10:50 +0800

    test: init integration test config

---
+++ src/test/java/com/drocketeers/server/config/BaseContainerEnv.java
@@ -0,0 +1,22 @@
+package com.drocketeers.server.config;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class PostgresContainerEnv {
+
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+}
